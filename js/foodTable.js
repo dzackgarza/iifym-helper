04cf3fb4f912147c0a04deeac79303da97b89b56
D. Zack Garza <dzackgarza@gmail.com>
2017-04-01 03:24:37 -0700

    Save progress

---
+++ js/foodTable.js
@@ -0,0 +1,701 @@
+foodLookup = {
+    "Arbys": [{
+        "name": "Roast Beef Classic Sandwich",
+        "calories": 360,
+        "carbs": 35,
+        "fat": 14,
+        "protein": 23
+    }, {
+        "name": "Prime-cut Chicken Tenders (3)",
+        "calories": 350,
+        "carbs": 25,
+        "fat": 17,
+        "protein": 25
+    }, {
+        "name": "Jr Roast Beef Sandwich",
+        "calories": 210,
+        "carbs": 22,
+        "fat": 8,
+        "protein": 13
+    }, {
+        "name": "Jr Bacon Cheddar Melt Sandwich",
+        "calories": 280,
+        "carbs": 25,
+        "fat": 12,
+        "protein": 17
+    }, {
+        "name": "Chopped Farmhouse Salad w/Roast Turkey",
+        "calories": 230,
+        "carbs": 8,
+        "fat": 13,
+        "protein": 23
+    }],
+    "Burger King": [{
+        "name": "Whopper Jr. Sandwich w/o Mayo",
+        "calories": 240,
+        "carbs": 27,
+        "fat": 10,
+        "protein": 10
+    }, {
+        "name": "Flame Broiled Hamburger",
+        "calories": 230,
+        "carbs": 26,
+        "fat": 9,
+        "protein": 9
+    }, {
+        "name": "Tendergrill Chicken Sandwich wo Mayo",
+        "calories": 350,
+        "carbs": 40,
+        "fat": 9,
+        "protein": 26
+    }, {
+        "name": "Ranch Grilled Chicken Wrap",
+        "calories": 310,
+        "carbs": 26,
+        "fat": 16,
+        "protein": 19
+    }, {
+        "name": "Tendergrill Chicken Apple Cranberry Salad w dressing",
+        "calories": 440,
+        "carbs": 34,
+        "fat": 26,
+        "protein": 28
+    }],
+    "Chick-Fil-A": [{
+        "name": "12-count Grilled Nuggets",
+        "calories": 200,
+        "carbs": 6,
+        "fat": 4.5,
+        "protein": 34
+    }, {
+        "name": "Grilled Chicken Sandwich",
+        "calories": 320,
+        "carbs": 40,
+        "fat": 5,
+        "protein": 30
+    }, {
+        "name": "Grilled Market Salad",
+        "calories": 200,
+        "carbs": 17,
+        "fat": 5,
+        "protein": 20
+    }, {
+        "name": "Asian Salad",
+        "calories": 330,
+        "carbs": 24,
+        "fat": 13,
+        "protein": 29
+    }, {
+        "name": "Grilled Chicken Club Sandwich",
+        "calories": 440,
+        "carbs": 41,
+        "fat": 14,
+        "protein": 38
+    }],
+    "Culvers": [{
+        "name": "Flame Roasted Chicken Sandwich",
+        "calories": 308,
+        "carbs": 36,
+        "fat": 9,
+        "protein": 27
+    }, {
+        "name": "Caesar Salad w Grilled Chicken",
+        "calories": 340,
+        "carbs": 14,
+        "fat": 16,
+        "protein": 34
+    }, {
+        "name": "George's Chili",
+        "calories": 300,
+        "carbs": 26,
+        "fat": 13,
+        "protein": 17
+    }, {
+        "name": "Chicken Noodle Soup",
+        "calories": 110,
+        "carbs": 14,
+        "fat": 2,
+        "protein": 7
+    }, {
+        "name": "Garden Fresco Salad w Grilled Chicken",
+        "calories": 360,
+        "carbs": 18,
+        "fat": 14,
+        "protein": 45
+    }],
+    "Firehouse Subs": [{
+        "name": "Sriracha Beef Sub w no mayo",
+        "calories": 408,
+        "carbs": 47,
+        "fat": 9.6,
+        "protein": 31.5
+    }, {
+        "name": "Hook & Ladder Light w no mayo",
+        "calories": 368,
+        "carbs": 50.8,
+        "fat": 8.6,
+        "protein": 29.8
+    }, {
+        "name": "Capt. Sorensen’s Datil Pepper Grilled Chicken Sub w no mayo",
+        "calories": 356,
+        "carbs": 51.3,
+        "fat": 7,
+        "protein": 29.6
+    }, {
+        "name": "Chopped Salad w Grilled Chicken and no dressing",
+        "calories": 248,
+        "carbs": 17.4,
+        "fat": 7.9,
+        "protein": 30.1
+    }, {
+        "name": "Chopped Salad w Turkey and no dressing",
+        "calories": 259,
+        "carbs": 18.9,
+        "fat": 8.4,
+        "protein": 31.3
+    }],
+    "Carls Jr.": [{
+        "name": "1/3Lb Low Carb Thickburger wo cheese or mayo",
+        "calories": 280,
+        "carbs": 9,
+        "fat": 16,
+        "protein": 19
+    }, {
+        "name": "Low Carb Charbroiled Chicken Club Sandwich wo mayo",
+        "calories": 210,
+        "carbs": 13,
+        "fat": 7,
+        "protein": 24
+    }, {
+        "name": "Trim It\" Charbroiled BBQ Chicken Sandwich",
+        "calories": 190,
+        "carbs": 24,
+        "fat": 3.5,
+        "protein": 18
+    }, {
+        "name": "Trim It\" Frisco Breakfast Sandwich",
+        "calories": 360,
+        "carbs": 44,
+        "fat": 11,
+        "protein": 19
+    }, {
+        "name": "Trim It\" 1/4Lb Low Carb Little Thickburger",
+        "calories": 220,
+        "carbs": 6,
+        "fat": 15,
+        "protein": 15
+    }],
+    "Hardees": [{
+        "name": "1/3Lb Low Carb Thickburger wo cheese or mayo",
+        "calories": 280,
+        "carbs": 9,
+        "fat": 16,
+        "protein": 19
+    }, {
+        "name": "Low Carb Charbroiled Chicken Club Sandwich wo mayo",
+        "calories": 210,
+        "carbs": 13,
+        "fat": 7,
+        "protein": 24
+    }, {
+        "name": "Trim It\" Charbroiled BBQ Chicken Sandwich",
+        "calories": 190,
+        "carbs": 24,
+        "fat": 3.5,
+        "protein": 18
+    }, {
+        "name": "Trim It\" Frisco Breakfast Sandwich",
+        "calories": 360,
+        "carbs": 44,
+        "fat": 11,
+        "protein": 19
+    }, {
+        "name": "Trim It\" 1/4Lb Low Carb Little Thickburger",
+        "calories": 220,
+        "carbs": 6,
+        "fat": 15,
+        "protein": 15
+    }],
+    "In-N-Out": [{
+        "name": "Hamburger w Protein Style Bun",
+        "calories": 240,
+        "carbs": 11,
+        "fat": 17,
+        "protein": 13
+    }, {
+        "name": "Hamburger w must & ketchup instead of spread",
+        "calories": 310,
+        "carbs": 41,
+        "fat": 10,
+        "protein": 16
+    }, {
+        "name": "Cheeseburger w Protein Style Bun",
+        "calories": 330,
+        "carbs": 11,
+        "fat": 25,
+        "protein": 18
+    }, {
+        "name": "Cheeseburger w must & ketchup instead of spread",
+        "calories": 400,
+        "carbs": 41,
+        "fat": 18,
+        "protein": 22
+    }, {
+        "name": "Double Double w must & ketchup instead of spread",
+        "calories": 590,
+        "carbs": 41,
+        "fat": 32,
+        "protein": 37
+    }],
+    "Jack in the Box": [{
+        "name": "Grilled Chicken Strips w Teriyaki Sauce",
+        "calories": 310,
+        "carbs": 15,
+        "fat": 8,
+        "protein": 44
+    }, {
+        "name": "Grilled Chicken Salad",
+        "calories": 250,
+        "carbs": 14,
+        "fat": 9,
+        "protein": 28
+    }, {
+        "name": "Egg White & Turkey Breakfast Sandwich",
+        "calories": 250,
+        "carbs": 31,
+        "fat": 6,
+        "protein": 18
+    }, {
+        "name": "Jr. Jack Burger",
+        "calories": 320,
+        "carbs": 33,
+        "fat": 15,
+        "protein": 14
+    }, {
+        "name": "Chicken Club Salad w Grilled Chicken Strips",
+        "calories": 360,
+        "carbs": 12,
+        "fat": 20,
+        "protein": 37
+    }, {
+        "name": "6. Southwest Salad w Grilled Chicken Strips",
+        "calories": 350,
+        "carbs": 28,
+        "fat": 15,
+        "protein": 34
+    }],
+    "KFC": [{
+        "name": "Crispy Chicken BLT Salad",
+        "calories": 350,
+        "carbs": 18,
+        "fat": 18,
+        "protein": 30
+    }, {
+        "name": "Crispy Chicken Caesar Salad",
+        "calories": 330,
+        "carbs": 16,
+        "fat": 17,
+        "protein": 29
+    }, {
+        "name": "Kentucky Grilled Chicken Breast",
+        "calories": 220,
+        "carbs": 0,
+        "fat": 7,
+        "protein": 40
+    }, {
+        "name": "Kentucky Grilled Chicken Drumstick",
+        "calories": 90,
+        "carbs": 0,
+        "fat": 4,
+        "protein": 13
+    }, {
+        "name": "Kentucky Grilled Chicken Wing",
+        "calories": 80,
+        "carbs": 0,
+        "fat": 4.5,
+        "protein": 10
+    }],
+    "Krystal Burger": [{
+        "name": "Krystal Chik",
+        "calories": 300,
+        "carbs": 27,
+        "fat": 16,
+        "protein": 14
+    }, {
+        "name": "Chik Club",
+        "calories": 320,
+        "carbs": 29,
+        "fat": 16,
+        "protein": 16
+    }, {
+        "name": "Small Chik'n Bitesl",
+        "calories": 200,
+        "carbs": 20,
+        "fat": 7,
+        "protein": 15
+    }, {
+        "name": "Large Chili",
+        "calories": 300,
+        "carbs": 33,
+        "fat": 11,
+        "protein": 17
+    }, {
+        "name": "Egg on Toast",
+        "calories": 230,
+        "carbs": 20,
+        "fat": 9,
+        "protein": 15
+    }],
+    "McDonalds": [{
+        "name": "Bacon Ranch Salad w Grilled Chicken",
+        "calories": 300,
+        "carbs": 9,
+        "fat": 14,
+        "protein": 37
+    }, {
+        "name": "Egg White Delight McMuffin",
+        "calories": 250,
+        "carbs": 30,
+        "fat": 8,
+        "protein": 18
+    }, {
+        "name": "Southwest Salad w Grilled Chicken",
+        "calories": 330,
+        "carbs": 26,
+        "fat": 11,
+        "protein": 32
+    }, {
+        "name": "Artisan Grilled Chicken Sandwich",
+        "calories": 360,
+        "carbs": 43,
+        "fat": 6,
+        "protein": 32
+    }, {
+        "name": "Vanilla Ice Cream Cone",
+        "calories": 170,
+        "carbs": 27,
+        "fat": 4.5,
+        "protein": 5
+    }],
+    "Panda Express": [{
+        "name": "Kung Pao Chicken",
+        "calories": 290,
+        "carbs": 14,
+        "fat": 19,
+        "protein": 16
+    }, {
+        "name": "Grilled Teriyaki Chicken",
+        "calories": 300,
+        "carbs": 8,
+        "fat": 13,
+        "protein": 36
+    }, {
+        "name": "Grilled Asian Chicken",
+        "calories": 300,
+        "carbs": 8,
+        "fat": 13,
+        "protein": 36
+    }, {
+        "name": "Teriyaki Chicken",
+        "calories": 380,
+        "carbs": 14,
+        "fat": 13,
+        "protein": 41
+    }, {
+        "name": "Broccoli Beef",
+        "calories": 150,
+        "carbs": 13,
+        "fat": 7,
+        "protein": 9
+    }],
+    "Popeyes Chicken & Biscuits": [{
+        "name": "Blackened Chicken Tenders (3)",
+        "calories": 170,
+        "carbs": 2,
+        "fat": 2,
+        "protein": 26
+    }, {
+        "name": "Blackened BBQ Chicken Po' Boy",
+        "calories": 340,
+        "carbs": 49,
+        "fat": 7,
+        "protein": 24
+    }, {
+        "name": "Mild Chicken Leg",
+        "calories": 160,
+        "carbs": 5,
+        "fat": 9,
+        "protein": 14
+    }, {
+        "name": "Spicy Chicken Leg",
+        "calories": 170,
+        "carbs": 5,
+        "fat": 10,
+        "protein": 13
+    }, {
+        "name": "Spicy Chicken Tenders",
+        "calories": 310,
+        "carbs": 16,
+        "fat": 15,
+        "protein": 28
+    }],
+    "Quiznos": [{
+        "name": "Small Turkey Sub wo cheese or dressing",
+        "calories": 320,
+        "carbs": 43,
+        "fat": 8,
+        "protein": 24
+    }, {
+        "name": "Large BBQ Ranch Chicken Salad wo cheese or dressing",
+        "calories": 220,
+        "carbs": 15,
+        "fat": 8,
+        "protein": 23
+    }, {
+        "name": "Large Honey Mustard Chicken Salad wo cheese or dressing",
+        "calories": 290,
+        "carbs": 13,
+        "fat": 14,
+        "protein": 28
+    }, {
+        "name": "Large Peppercorn Caesar Salad wo cheese or dressing",
+        "calories": 180,
+        "carbs": 13,
+        "fat": 6,
+        "protein": 22
+    }, {
+        "name": "Small Basil Pesto Chicken Grilled Flatbread wo cheese or dressing",
+        "calories": 360,
+        "carbs": 37,
+        "fat": 15,
+        "protein": 20
+    }, {
+        "name": "Sonic Drive-In",
+        "calories": "In",
+        "carbs": "",
+        "fat": "",
+        "protein": ""
+    }, {
+        "name": "Jr Burger",
+        "calories": 330,
+        "carbs": 30,
+        "fat": 17,
+        "protein": 15
+    }, {
+        "name": "Chicken Strips Kids Meal",
+        "calories": 220,
+        "carbs": 17,
+        "fat": 11,
+        "protein": 14
+    }, {
+        "name": "Classic Grilled Chicken Sandwich",
+        "calories": 450,
+        "carbs": 44,
+        "fat": 17,
+        "protein": 32
+    }, {
+        "name": "Jr Breakfast Burrito",
+        "calories": 230,
+        "carbs": 23,
+        "fat": 15,
+        "protein": 13
+    }, {
+        "name": "Vanilla Ice Cream (cup)",
+        "calories": 240,
+        "carbs": 26,
+        "fat": 13,
+        "protein": 4
+    }],
+    "SUBWAY®Restaurants": [{
+        "name": "6\" Roast Beef",
+        "calories": 320,
+        "carbs": 45,
+        "fat": 5,
+        "protein": 24
+    }, {
+        "name": "6\" Oven Roasted Chicken",
+        "calories": 320,
+        "carbs": 47,
+        "fat": 5,
+        "protein": 23
+    }, {
+        "name": "6\" Turkey Breast",
+        "calories": 280,
+        "carbs": 46,
+        "fat": 3.5,
+        "protein": 18
+    }, {
+        "name": "6\" Monterey Chicken Melt",
+        "calories": 360,
+        "carbs": 45,
+        "fat": 8,
+        "protein": 28
+    }, {
+        "name": "6\" Steak, Egg White & Cheese",
+        "calories": 390,
+        "carbs": 47,
+        "fat": 10,
+        "protein": 28
+    }],
+    "Taco Bell": [{
+        "name": "Cantina Power Chicken Burrito",
+        "calories": 450,
+        "carbs": 43,
+        "fat": 19,
+        "protein": 28
+    }, {
+        "name": "Cantina Chicken Power Bowl",
+        "calories": 490,
+        "carbs": 49,
+        "fat": 20,
+        "protein": 29
+    }, {
+        "name": "Fresco Soft Chicken Taco",
+        "calories": 140,
+        "carbs": 16,
+        "fat": 3.5,
+        "protein": 10
+    }, {
+        "name": "Fresco Style Fiesta Taco Salad wo beans or rice",
+        "calories": 400,
+        "carbs": 41,
+        "fat": 16,
+        "protein": 23
+    }, {
+        "name": "Fresco Cool Ranch Doritos Locos Taco w grilled chicken",
+        "calories": 150,
+        "carbs": 12,
+        "fat": 6,
+        "protein": 11
+    }],
+    "Wendys": [{
+        "name": "Full Size Apple Pecan Chicken Salad",
+        "calories": 350,
+        "carbs": 28,
+        "fat": 11,
+        "protein": 35
+    }, {
+        "name": "Full Size Asian Cashew Chicken Salad",
+        "calories": 210,
+        "carbs": 14,
+        "fat": 3.5,
+        "protein": 33
+    }, {
+        "name": "Large Chili",
+        "calories": 250,
+        "carbs": 23,
+        "fat": 7,
+        "protein": 23
+    }, {
+        "name": "Spicy Chicken Nuggets (4pc)",
+        "calories": 170,
+        "carbs": 9,
+        "fat": 11,
+        "protein": 10
+    }, {
+        "name": "Grilled Chicken Sandwich",
+        "calories": 370,
+        "carbs": 43,
+        "fat": 7,
+        "protein": 34
+    }, {
+        "name": "6. Grilled Chicken Go Wrap",
+        "calories": 270,
+        "carbs": 25,
+        "fat": 3.5,
+        "protein": 19
+    }],
+    "Whataburger": [{
+        "name": "Whataburger Jr",
+        "calories": 310,
+        "carbs": 37,
+        "fat": 11,
+        "protein": 14
+    }, {
+        "name": "Grilled Chicken Sandwich",
+        "calories": 430,
+        "carbs": 49,
+        "fat": 13,
+        "protein": 35
+    }, {
+        "name": "Garden Salad",
+        "calories": 160,
+        "carbs": 10,
+        "fat": 10,
+        "protein": 10
+    }, {
+        "name": "Double Meat Whataburger Jr",
+        "calories": 420,
+        "carbs": 37,
+        "fat": 20,
+        "protein": 23
+    }, {
+        "name": "Chicken Fajita Taco",
+        "calories": 340,
+        "carbs": 29,
+        "fat": 12,
+        "protein": 28
+    }],
+    "White Castle": [{
+        "name": "Original Slider",
+        "calories": 140,
+        "carbs": 13,
+        "fat": 6,
+        "protein": 7
+    }, {
+        "name": "Grilled Chicken Slider",
+        "calories": 180,
+        "carbs": 13,
+        "fat": 7,
+        "protein": 15
+    }, {
+        "name": "Plain Veggie Slider",
+        "calories": 150,
+        "carbs": 20,
+        "fat": 5,
+        "protein": 5
+    }, {
+        "name": "Egg & Cheese Breakfast Slider",
+        "calories": 160,
+        "carbs": 13,
+        "fat": 7,
+        "protein": 9
+    }, {
+        "name": "Egg & Hamburger Breakfast Slider",
+        "calories": 190,
+        "carbs": 12,
+        "fat": 11,
+        "protein": 12
+    }],
+    "Zaxbys": [{
+        "name": "Grilled Chicken House Salad wo toast, fried onions, dressing",
+        "calories": 308,
+        "carbs": 9,
+        "fat": 12.5,
+        "protein": 40
+    }, {
+        "name": "Grilled Chicken Caesar Salad wo dressing",
+        "calories": 540,
+        "carbs": 31,
+        "fat": 24.5,
+        "protein": 49
+    }, {
+        "name": "The Blue Blackened wo toast, friend onions dressing",
+        "calories": 336,
+        "carbs": 14,
+        "fat": 12.5,
+        "protein": 42
+    }, {
+        "name": "Buffalo Fingers wo sauce",
+        "calories": 390,
+        "carbs": 13,
+        "fat": 18,
+        "protein": 44
+    }, {
+        "name": "Chicken Salad Zalad wo toast",
+        "calories": 260,
+        "carbs": 10,
+        "fat": 17,
+        "protein": 17
+    }]
+}
